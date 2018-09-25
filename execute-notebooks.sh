4c03382595a6679e52447c4af91dd53a7f88dbdf
arnavs <soodarnav01@gmail.com>
2018-09-25 12:13:59 -0700

    finalize execution script

--- execute-notebooks.sh
+++ execute-notebooks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-filename = $1 # Grab our input. 
-\cp ./_build/jupyter/filename ./notebooks/filename # Move it to the right folder. Will overwrite. 
-jupyter nbconvert --ExecutePreprocessor.timeout=600 --to notebook --execute filename # Runs the file. 
+redln() { echo -n "$(tput setaf 1)$*$(tput setaf 9)"; } # Prints in red. For error messages. 
+if [ $# -eq 0 ]; then
+    redln "No arguments supplied. Will exit now."
+    exit 1
+fi
+
+# Otherwise. 
+\cp ./_build/jupyter/$1 ./notebooks/$1 # Move first argument to the right folder. Will overwrite. 
+jupyter nbconvert ./notebooks/$1 --ExecutePreprocessor.timeout=600 --to notebook --inplace --allow-errors --execute  # Runs the file. 
