bce75a718063f515002a912389b9a897d966b7c8
arnavs <soodarnav01@gmail.com>
2018-09-25 15:58:01 -0700

    add unicode file

---
+++ unicode.jl
@@ -0,0 +1,15 @@
+# Goal of this file is to test unicode support across platforms we'll be using (Atom, Terminal, Jupyter) and across OSs.
+
+# Glyphs
+    L̃ = 2.0 # L-tilde
+    u₀ = 3.0 # u-naught
+    â = 4.0 # a-hat
+    z̲ = 5.0 # z-underbar
+
+# Ordinary unicode characters
+    println("π, α, ζ, ξ, δ, ρ, γ, β, χ, ω, θ, υ, ν, Γ, Δ")
+
+# Combining.
+    "γ̃" # gamma-tilde
+    "α̂" # alpha-hat
+    "π̲" # pi-underbar
