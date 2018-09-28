3c747179c3414a5923be56c545ce209f183bbe13
arnavs <soodarnav01@gmail.com>
2018-09-28 11:34:24 -0700

    cosmetics

--- run-all-notebooks.sh
+++ run-all-notebooks.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-redln() { echo "$(tput setaf 1)$*$(tput setaf 9)"; } # Prints in red. For error messages. 
+red=`tput setaf 1` # For errors
+green=`tput setaf 2`
+blue=`tput setaf 4`
 
 for f in _build/jupyter/*.ipynb; do
-  lec=$(basename $f) # For ease of use 
+  lec=$(basename $f) # For ease of use
+  echo "${blue} lecture $lec"
   # Check if the target exists, and if it doesn't, add it. 
   if [ -e ./notebooks/"$lec" ]
   then
-    :
+    echo "${green} target exists; checking timestamps..."
   else 
-    echo "target does not exist; creating now."
+    echo "${green} target does not exist; creating now."
     \cp "$f" ./notebooks/"$lec" # Move first argument to the right folder. Will overwrite. 
     jupyter nbconvert ./notebooks/"$lec" --ExecutePreprocessor.timeout=600 --to notebook --inplace --allow-errors --execute  # Runs the file. 
     continue # Move on to the next file. 
   fi 
   # Check if our file is newer than the target. If it is, copy and overwrite. 
-  if [["$f" -nt ./notebooks/"$lec"]]
+  if [[ "$f" -nt ./notebooks/"$lec" ]]
   then
+    echo "${green} replacing now"
     \cp "$f" ./notebooks/"$lec" # Move first argument to the right folder. Will overwrite. 
     jupyter nbconvert ./notebooks/"$lec" --ExecutePreprocessor.timeout=600 --to notebook --inplace --allow-errors --execute  # Runs the file. 
-  fi 
+    echo "${green} conversion complete"
+  else
+    echo "${green} target is not older than source."
+  fi
 done 
