a73afcc3f7f5935543ac48a2999bae4432657f66
arnavs <soodarnav01@gmail.com>
2018-09-28 11:48:13 -0700

    cosmetics 2

--- run-all-notebooks.sh
+++ run-all-notebooks.sh
@@ -14,7 +14,8 @@ for f in _build/jupyter/*.ipynb; do
   else 
     echo "${green} target does not exist; creating now."
     \cp "$f" ./notebooks/"$lec" # Move first argument to the right folder. Will overwrite. 
-    jupyter nbconvert ./notebooks/"$lec" --ExecutePreprocessor.timeout=600 --to notebook --inplace --allow-errors --execute  # Runs the file. 
+    jupyter nbconvert ./notebooks/"$lec" --ExecutePreprocessor.timeout=600 --to notebook --inplace --allow-errors --execute  # Runs the file.
+    echo "${green} conversion complete."
     continue # Move on to the next file. 
   fi 
   # Check if our file is newer than the target. If it is, copy and overwrite. 
