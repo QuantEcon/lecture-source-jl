91751c6dd6651980d9232141923991462e73fad1
Natasha <tashawatkins@gmail.com>
2018-11-13 14:00:30 +1100

    remove spaces around tag in deps.jl
    Is this standard? I'm used to seeing no spaces around keyword arguments

--- rst_files/_static/includes/deps.jl
+++ rst_files/_static/includes/deps.jl
@@ -1,6 +1,6 @@
 using InstantiateFromURL
 
 # activate the QuantEcon environment
-activate_github("QuantEcon/QuantEconLecturePackages", tag = "v0.9.0") 
+activate_github("QuantEcon/QuantEconLecturePackages", tag="v0.9.0") 
 
 using LinearAlgebra, Statistics, Compat # load common packages
