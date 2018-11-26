b62738ae21cd39438c8e106e2469c61090f307e2
Arnav Sood <soodarnav01@gmail.com>
2018-11-26 13:48:30 -0800

    add ; to activate_github() calls

--- rst_files/_static/includes/deps.jl
+++ rst_files/_static/includes/deps.jl
@@ -1,7 +1,7 @@
 using InstantiateFromURL
 
 # activate the QuantEcon environment
-activate_github("QuantEcon/QuantEconLecturePackages", tag = "v0.9.4") 
+activate_github("QuantEcon/QuantEconLecturePackages", tag = "v0.9.4");
 
 # load common packages
 using LinearAlgebra, Statistics, Compat 
