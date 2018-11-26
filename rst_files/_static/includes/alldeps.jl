b577bb42d6cd02d43f3ad53b77e5a486c491f6c8
Arnav Sood <soodarnav01@gmail.com>
2018-11-26 13:48:45 -0800

    stragglers on last commit

--- rst_files/_static/includes/alldeps.jl
+++ rst_files/_static/includes/alldeps.jl
@@ -1,7 +1,7 @@
 using InstantiateFromURL
 
 # activate the QuantEcon environment
-activate_github("QuantEcon/QuantEconLectureAllPackages", tag = "v0.9.4")
+activate_github("QuantEcon/QuantEconLectureAllPackages", tag = "v0.9.4");
 
 # load common packages
 using LinearAlgebra, Statistics, Compat 
--- rst_files/_static/includes/deps_no_using.jl
+++ rst_files/_static/includes/deps_no_using.jl
@@ -1,4 +1,4 @@
 using InstantiateFromURL
 
 # activate the QuantEcon environment
-activate_github("QuantEcon/QuantEconLecturePackages", tag = "v0.9.4") 
+activate_github("QuantEcon/QuantEconLecturePackages", tag = "v0.9.4");
