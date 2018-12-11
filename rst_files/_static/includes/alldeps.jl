cbaff3cd2ce427092881d3e8161b26b2b47e8e47
Arnav Sood <soodarnav01@gmail.com>
2018-12-11 12:55:45 -0800

    delete old deps.jl and alldeps.jl

--- rst_files/_static/includes/alldeps.jl
+++
@@ -1,7 +0,0 @@
-using InstantiateFromURL
-
-# activate the QuantEcon environment
-activate_github("QuantEcon/QuantEconLectureAllPackages", tag = "v0.9.5");
-
-# load common packages
-using LinearAlgebra, Statistics, Compat
