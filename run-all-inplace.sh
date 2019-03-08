e8d26300d09f55eaa03cafb012f2cc83b619a2a7
Arnav Sood <soodarnav01@gmail.com>
2019-03-08 08:06:25 -0800

    Update run-all-inplace.sh

--- run-all-inplace.sh
+++ run-all-inplace.sh
@@ -4,9 +4,6 @@ red=`tput setaf 1` # For errors
 green=`tput setaf 2`
 blue=`tput setaf 4`
 
-make clean
-make jupyter
-
 for f in $(find _build/jupyter -name "*.ipynb"); do
   lec=$(basename $f) # For ease of use
   if [[ $lec =~ .*index.* ]]; then
