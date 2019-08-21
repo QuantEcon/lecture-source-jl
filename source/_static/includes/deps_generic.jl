736f61861e1b26c917dc1042f55df5fb01686777
Arnav Sood <me@arnavsood.com>
2019-08-21 08:17:20 -0700

    add line to activation header about instantiate if required

--- source/_static/includes/deps_generic.jl
+++ source/_static/includes/deps_generic.jl
@@ -1,2 +1,3 @@
 using InstantiateFromURL
 github_project("QuantEcon/quantecon-notebooks-jl", version = "v0.9.10");
+# ] instantiate  # uncomment if required
--- source/_static/includes/deps_getting_started_julia.jl
+++ source/_static/includes/deps_getting_started_julia.jl
@@ -1,2 +1,3 @@
 using InstantiateFromURL
 activate_github("QuantEcon/quantecon-notebooks-jl", path = "getting_started_julia", version = "v0.9.10");
+# ] instantiate  # uncomment if required
