7ad68fa5590b3a382ea9f7ec2bcf53e353dfb047
Jesse Perla <jesseperla@gmail.com>
2020-04-09 09:34:39 -0700

    Better spacing for web layout (#805)
    @arnavs Can you check this out and merge if you think it works? Maybe look for any other places that it might show up

--- source/_static/includes/deps_generic.jl
+++ source/_static/includes/deps_generic.jl
@@ -1,3 +1,4 @@
 using InstantiateFromURL
 github_project("QuantEcon/quantecon-notebooks-julia", version = "0.5.0")
-# github_project("QuantEcon/quantecon-notebooks-julia", version = "0.5.0", instantiate = true) # uncomment to force package installation
+# uncomment to force package installation
+# github_project("QuantEcon/quantecon-notebooks-julia", version = "0.5.0", instantiate = true)
