22fb66720e0759434004d37f796a009eea02e263
Arnav Sood <5009477+arnavs@users.noreply.github.com>
2020-05-14 10:46:18 -0700

    bump things to v0.6.0

--- source/_static/includes/deps_generic.jl
+++ source/_static/includes/deps_generic.jl
@@ -1,4 +1,4 @@
 using InstantiateFromURL
-github_project("QuantEcon/quantecon-notebooks-julia", version = "0.5.0")
+github_project("QuantEcon/quantecon-notebooks-julia", version = "0.6.0")
 # uncomment to force package installation
-# github_project("QuantEcon/quantecon-notebooks-julia", version="0.5.0", instantiate=true)
+# github_project("QuantEcon/quantecon-notebooks-julia", version="0.6.0", instantiate=true)
