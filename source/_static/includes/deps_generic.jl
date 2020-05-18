41831388771f92ae1fcd1d5e9467d2e6b1e25e7e
Jesse Perla <jesseperla@gmail.com>
2020-05-18 14:17:39 -0700

    Width of instantiate cell added scrollbars again Fixes #851

--- source/_static/includes/deps_generic.jl
+++ source/_static/includes/deps_generic.jl
@@ -1,4 +1,3 @@
 using InstantiateFromURL
+# add arguments "instantiate=true, precompile = true" to force installation 
 github_project("QuantEcon/quantecon-notebooks-julia", version = "0.7.0")
-# uncomment to force package installation and precompilation
-# github_project("QuantEcon/quantecon-notebooks-julia", version="0.7.0", instantiate=true, precompile = true)
