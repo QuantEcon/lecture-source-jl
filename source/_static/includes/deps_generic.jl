6b30b3874d36990ffb47701391a1471a841ac4de
Jesse Perla <jesseperla@gmail.com>
2020-05-18 14:26:58 -0700

    Change in wording

--- source/_static/includes/deps_generic.jl
+++ source/_static/includes/deps_generic.jl
@@ -1,3 +1,3 @@
 using InstantiateFromURL
-# To force installation, add arguments: instantiate = true, precompile = true
+# optionally add arguments to force installation: instantiate = true, precompile = true
 github_project("QuantEcon/quantecon-notebooks-julia", version = "0.7.0")
